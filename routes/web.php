14490d2b45794f50004de359eb37b7883ae84e3c
juliano <jot.asa@hotmail.com>
2023-06-26 20:09:43 -0300

    Breeze_install

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\BoardController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\WoodController;
-use App\Models\Board;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,27 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Home
-Route::get('home', [BoardController::class, 'start'])->name('start');
-Route::view('busca', 'search')->name('search');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Cliente
-Route::get('cliente', [BoardController::class, 'indexclient'])->name('index.client');
-Route::post('cliente', [BoardController::class, 'storeclient'])->name('store.client');
-Route::get('busca/cliente', [BoardController::class, 'searchclient'])->name('search.client');
-Route::get('cliente/{id}', [BoardController::class, 'showclient'])->name('show.client');
-Route::get('cliente/{id}/edit', [BoardController::class, 'editclient'])->name('edit.client');
-Route::put('cliente/{id}', [BoardController::class, 'updateclient'])->name('update.client');
-Route::delete('cliente/{id}', [BoardController::class, 'destroyclient'])->name('destroy.client');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//Colaborador
-Route::get('funcionario', [EmployeeController::class, 'indexemployee'])->name('index.employee');
-Route::post('funcionario', [EmployeeController::class, 'storeemployee'])->name('store.employee');
-Route::get('busca/funcionario', [EmployeeController::class, 'searchemployee'])->name('search.employee');
-Route::get('funcionario/{id}', [EmployeeController::class, 'showemployee'])->name('show.employee');
-Route::get('funcionario/{id}/edit', [EmployeeController::class, 'editemployee'])->name('edit.employee');
-Route::put('funcionario/{id}', [EmployeeController::class, 'updateemployee'])->name('update.employee');
-Route::delete('funcionario/{id}', [EmployeeController::class, 'destroyemployee'])->name('destroy.employee');
-
-//Usuário
-Route::get('usuario', [UserController::class, 'indexuser'])->name('index.user');
+require __DIR__.'/auth.php';
