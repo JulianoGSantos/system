3d61d9a5ca8f73511975da69c47581f51a829217
juliano <jot.asa@hotmail.com>
2023-06-29 18:32:54 -0300

    breeze_ok

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
