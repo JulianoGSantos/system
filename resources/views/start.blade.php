0853a45f30a9259a46791994c261d9ca2c417801
juliano <jot.asa@hotmail.com>
2023-06-30 19:03:57 -0300

    Auth_first_screen

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -15,6 +15,13 @@
     <a href=" {{route ('index.user') }}">Usuário</a>
 </body>
 <footer>
+    <script src="https://cdn.tailwindcss.com"></script>
     <a href=" {{ route('search') }}">Busca</a>
+    <div class="container mx-auto px-4 py-8">
+        <form action=" {{ route('logout') }}" method="POST" class="shadow bg-purple-500 hover:bg-purple-40 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+        @csrf
+        <button type="submit">Logout</button>
+        </form>
+    </div>
 </footer>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,29 +21,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Home
-Route::get('home', [BoardController::class, 'start'])->name('start');
-Route::view('busca', 'search')->name('search');
+Route::middleware(['auth'])->group(function(){
+    
+    //Home
+    Route::get('home', [BoardController::class, 'start'])->name('start');
+    Route::view('busca', 'search')->name('search');
 
-//Cliente
-Route::get('cliente', [BoardController::class, 'indexclient'])->name('index.client');
-Route::post('cliente', [BoardController::class, 'storeclient'])->name('store.client');
-Route::get('busca/cliente', [BoardController::class, 'searchclient'])->name('search.client');
-Route::get('cliente/{id}', [BoardController::class, 'showclient'])->name('show.client');
-Route::get('cliente/{id}/edit', [BoardController::class, 'editclient'])->name('edit.client');
-Route::put('cliente/{id}', [BoardController::class, 'updateclient'])->name('update.client');
-Route::delete('cliente/{id}', [BoardController::class, 'destroyclient'])->name('destroy.client');
+    //Cliente
+    Route::get('cliente', [BoardController::class, 'indexclient'])->name('index.client');
+    Route::post('cliente', [BoardController::class, 'storeclient'])->name('store.client');
+    Route::get('busca/cliente', [BoardController::class, 'searchclient'])->name('search.client');
+    Route::get('cliente/{id}', [BoardController::class, 'showclient'])->name('show.client');
+    Route::get('cliente/{id}/edit', [BoardController::class, 'editclient'])->name('edit.client');
+    Route::put('cliente/{id}', [BoardController::class, 'updateclient'])->name('update.client');
+    Route::delete('cliente/{id}', [BoardController::class, 'destroyclient'])->name('destroy.client');
 
-//Colaborador
-Route::get('funcionario', [EmployeeController::class, 'indexemployee'])->name('index.employee');
-Route::post('funcionario', [EmployeeController::class, 'storeemployee'])->name('store.employee');
-Route::get('busca/funcionario', [EmployeeController::class, 'searchemployee'])->name('search.employee');
-Route::get('funcionario/{id}', [EmployeeController::class, 'showemployee'])->name('show.employee');
-Route::get('funcionario/{id}/edit', [EmployeeController::class, 'editemployee'])->name('edit.employee');
-Route::put('funcionario/{id}', [EmployeeController::class, 'updateemployee'])->name('update.employee');
-Route::delete('funcionario/{id}', [EmployeeController::class, 'destroyemployee'])->name('destroy.employee');
+    //Colaborador
+    Route::get('funcionario', [EmployeeController::class, 'indexemployee'])->name('index.employee');
+    Route::post('funcionario', [EmployeeController::class, 'storeemployee'])->name('store.employee');
+    Route::get('busca/funcionario', [EmployeeController::class, 'searchemployee'])->name('search.employee');
+    Route::get('funcionario/{id}', [EmployeeController::class, 'showemployee'])->name('show.employee');
+    Route::get('funcionario/{id}/edit', [EmployeeController::class, 'editemployee'])->name('edit.employee');
+    Route::put('funcionario/{id}', [EmployeeController::class, 'updateemployee'])->name('update.employee');
+    Route::delete('funcionario/{id}', [EmployeeController::class, 'destroyemployee'])->name('destroy.employee');
 
-//Usuário
-Route::get('usuario', [UserController::class, 'indexuser'])->name('index.user');
+    //Usuário
+    Route::get('usuario', [UserController::class, 'indexuser'])->name('index.user');
+});
+
 
 require __DIR__.'/auth.php';
