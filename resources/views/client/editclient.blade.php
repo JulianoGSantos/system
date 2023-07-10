4e2606f60868307ba8c943846de0a8fb183e71db
juliano <jot.asa@hotmail.com>
2023-07-10 19:43:31 -0300

    tailwind_part8

--- resources/views/client/editclient.blade.php
+++ resources/views/client/editclient.blade.php
@@ -10,38 +10,46 @@
 <header>
     <div>EDIÇÃO DE CLIENTES</div>
 </header>
-<body>
-    <form action=" {{ route('update.client', $client->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-        <label for="iname">Nome</label>
-        <input type="text" name="name" id="iname" value="{{$client->name}}">
-        <label for="icpf">CPF</label>
-        <input type="text" name="cpf" id="icpf" value="{{$client->cpf}}">
-        <label for="iemail">E-mail</label>
-        <input type="text" name="email" id="iemail" value="{{$client->email}}">
-        <div>Endereço</div>
-        <input type="text" name="street" id="istreet" value="{{$client->street}}">
-        <label for="istreet">Rua</label>
-        <input type="text" name="number" id="inumber" value="{{$client->number}}">
-        <label for="inumber">Número</label>
-        <input type="text" name="complement" id="icomplement" value="{{$client->complement}}">
-        <label for="icomplement">Complemento</label>
-        <input type="text" name="city" id="icity" value="{{$client->city}}">
-        <label for="icity">Cidade</label>
-        <input type="text" name="state" id="istate" value="{{$client->state}}">
-        <label for="istate">Estado</label>
-        <input type="text" name="cep" id="icep" value="{{$client->cep}}">
-        <label for="icep">CEP</label>
-        <div>Telefone</div>
-        <input type="text" name="cellphone" id="icellphone" value="{{$client->cellphone}}">
-        <label for="icellphone">Celular</label>
-        <input type="text" name="cellphone2" id="icellphone2" value="{{$client->cellphone2}}">
-        <label for="icellphone2">Celular2</label>
-        <button type="submit">Atualizar</button>
-    </form>
-</body>
-<footer>
-    <div><a href="http://www.google.com">google</a></div>
-</footer>
+@section('content')
+    <body>
+        <form action=" {{ route('update.client', $client->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="grid grid-cols-2">
+            <label for="iname">Nome</label>
+            <input type="text" name="name" id="iname" value="{{$client->name}}">
+            <label for="icpf">CPF</label>
+            <input type="text" name="cpf" id="icpf" value="{{$client->cpf}}">
+            <label for="iemail">E-mail</label>
+            <input type="text" name="email" id="iemail" value="{{$client->email}}">
+            <div>Endereço</div>
+            <input type="text" name="street" id="istreet" value="{{$client->street}}">
+            <label for="istreet">Rua</label>
+            <input type="text" name="number" id="inumber" value="{{$client->number}}">
+            <label for="inumber">Número</label>
+            <input type="text" name="complement" id="icomplement" value="{{$client->complement}}">
+            <label for="icomplement">Complemento</label>
+            <input type="text" name="city" id="icity" value="{{$client->city}}">
+            <label for="icity">Cidade</label>
+            <input type="text" name="state" id="istate" value="{{$client->state}}">
+            <label for="istate">Estado</label>
+            <input type="text" name="cep" id="icep" value="{{$client->cep}}">
+            <label for="icep">CEP</label>
+            <div>Telefone</div>
+            <input type="text" name="cellphone" id="icellphone" value="{{$client->cellphone}}">
+            <label for="icellphone">Celular</label>
+            <input type="text" name="cellphone2" id="icellphone2" value="{{$client->cellphone2}}">
+            <label for="icellphone2">Celular2</label>
+            <div class="mt-4 mb-5">
+                <button type="submit" class="bg-red-400 px-4 py-1 rounded-xl text-white ring-1">Atualizar</button>
+            </div>
+        </div>
+        </form>
+    </body>
+@endsection
+@section('footer')
+    <footer>
+        <div class="text-center text-blue-600"><a href="{{ route('start') }}">página inicial</a></div>
+    </footer>
+@endsection
 </html>
